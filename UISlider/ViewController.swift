e891309b411a743f252196ad599450f42dc6bbef
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2017-01-21 01:05:23 +0530

    UISlider is the basic UI component that provides user with the option to select the point from continuous range of points.

--- UISlider/ViewController.swift
+++ UISlider/ViewController.swift
@@ -9,11 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var rangeSlider = UISlider()
+     var myLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myLabel.frame = CGRect(x: 100, y: 200, width: 200, height: 20)
+        view.addSubview(myLabel)
+        
+        
+        rangeSlider = UISlider(frame:CGRect(x: 20, y: 260, width: 280, height: 20))
+        rangeSlider.minimumValue = 0
+        rangeSlider.maximumValue = 100
+        rangeSlider.isContinuous = true
+        rangeSlider.tintColor = UIColor.red
+        rangeSlider.value = 50
+        rangeSlider.addTarget (self,action: #selector(numberValueChanged), for: UIControlEvents.valueChanged
+        )
+        self.view.addSubview(rangeSlider)
     }
+    
+    func numberValueChanged(sender: UISlider) {
+        rangeSlider.setValue(Float(Int(rangeSlider.value)), animated: true)
+        let v = rangeSlider.value
+        self.myLabel.text = "Slider Value : "+"\(v)"
+    }
+    
+
+
+
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
